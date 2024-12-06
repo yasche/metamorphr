60c0a4138f058340cb41839b865154260fe85f33
yasche <172521093+yasche@users.noreply.github.com>
2024-12-06 16:20:18 +0100

    added impute_lod and documentation

--- R/imputes.R
+++ R/imputes.R
@@ -39,7 +39,7 @@ impute_global_lowest <- function(data) {
 
 impute_user_value <- function(data, value) {
   data %>%
-    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ value,
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ .env$value,
                                                .default = .data$Intensity))
 }
 
@@ -117,8 +117,31 @@ impute_min <- function(data) {
     dplyr::select(-"LoD")
 }
 
-impute_lod <- function() {
-
+#' Impute missing values by replacing them with the Feature 'Limit of Detection'
+#'
+#' @description
+#' Replace missing intensity values (`NA`) by what is assumed to be the detector limit of detection (LoD).
+#' It is estimated by dividing the Feature minimum by the provided denominator, usually 5. See the References section for more information.
+#'
+#' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
+#' @param denominator A numeric value that specifies by which the Feature minimum will be divided
+#'
+#' @return A tibble with imputed missing values.
+#' @references <a href="https://omicsforum.ca/t/how-to-deal-with-missing-values/75">LoD on OmicsForum</a>
+#' @export
+#'
+#' @examples
+#' toy_metaboscape %>%
+#'   impute_lod()
+impute_lod <- function(data, denominator = 5) {
+  #https://omicsforum.ca/t/how-to-deal-with-missing-values/75
+  data %>%
+    dplyr::group_by(.data$UID) %>%
+    dplyr::mutate(LoD = min(.data$Intensity, na.rm = T) / .env$denominator) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ .data$LoD,
+                                               .default = .data$Intensity)) %>%
+    dplyr::select(-"LoD")
 }
 
 impute_knn <- function() {
