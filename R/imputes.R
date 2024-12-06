aaa39479156153be03ebe60d36f934c0a909d793
yasche <172521093+yasche@users.noreply.github.com>
2024-12-06 14:59:13 +0100

    added impute_mean and documentation

--- R/imputes.R
+++ R/imputes.R
@@ -1,7 +1,7 @@
 #' Impute missing values by replacing them with the lowest observed intensity (global)
 #'
 #' @description
-#' Replace missing intensity values (`NA`) with the lowest observed intensity. This imputation method is very rudimentary and other methods might provide better results.
+#' Replace missing intensity values (`NA`) with the lowest observed intensity.
 #'
 #' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
 #'
@@ -25,7 +25,7 @@ impute_global_lowest <- function(data) {
 #' Impute missing values by replacing them with a user-provided value
 #'
 #' @description
-#' Replace missing intensity values (`NA`) with a user-provided value (e.g., 1). This imputation method is very rudimentary and other methods might provide better results.
+#' Replace missing intensity values (`NA`) with a user-provided value (e.g., 1).
 #'
 #' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
 #' @param value Numeric that replaces missing values
@@ -43,8 +43,29 @@ impute_user_value <- function(data, value) {
                                                .default = .data$Intensity))
 }
 
-impute_mean <- function() {
+#' Impute missing values by replacing them with the Feature mean
+#'
+#' @description
+#' Replace missing intensity values (`NA`) with the Feature mean of non-`NA` values. For example, if a Feature has the measured intensities `NA, 1, NA, 3` in samples 1-4,
+#' the intensities after `impute_mean()` would be `2, 1, 2, 3`.
+#'
+#' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
+#'
+#' @return A tibble with imputed missing values.
+#' @export
+#'
+#' @examples
+#' toy_metaboscape %>%
+#'   impute_mean()
 
+impute_mean <- function(data) {
+  data %>%
+    dplyr::group_by(.data$UID) %>%
+    dplyr::mutate(LoD = mean(.data$Intensity, na.rm = T)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ .data$LoD,
+                                               .default = .data$Intensity)) %>%
+    dplyr::select(-"LoD")
 }
 
 impute_median <- function() {
