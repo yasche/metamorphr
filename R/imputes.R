30b06549c28e4ad51533c0c1179a361a14dc9d9f
yasche <172521093+yasche@users.noreply.github.com>
2024-12-06 14:19:57 +0100

    added impute_user_value and documentation

--- R/imputes.R
+++ R/imputes.R
@@ -1,7 +1,7 @@
 #' Impute missing values by replacing them with the lowest observed intensity (global)
 #'
 #' @description
-#' Replace missing intensity values (`NA`) with the lowest observed intensity. This imputation method is very rudimentary.
+#' Replace missing intensity values (`NA`) with the lowest observed intensity. This imputation method is very rudimentary and other methods might provide better results.
 #'
 #' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
 #'
@@ -11,17 +11,36 @@
 #' @examples
 #' toy_metaboscape %>%
 #'   impute_global_lowest()
-#'
+
 impute_global_lowest <- function(data) {
   data %>%
     dplyr::mutate(LoD = min(.data$Intensity, na.rm = T)) %>%
-    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ LoD,
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ .data$LoD,
                                                .default = .data$Intensity)) %>%
-    dplyr::select(-LoD)
+    dplyr::select(-"LoD")
 }
 
-impute_user_value <- function() {
 
+
+#' Impute missing values by replacing them with a user-provided value
+#'
+#' @description
+#' Replace missing intensity values (`NA`) with a user-provided value (e.g., 1). This imputation method is very rudimentary and other methods might provide better results.
+#'
+#' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
+#' @param value Numeric that replaces missing values
+#'
+#' @return A tibble with imputed missing values.
+#' @export
+#'
+#' @examples
+#' toy_metaboscape %>%
+#'   impute_user_value(value = 1)
+
+impute_user_value <- function(data, value) {
+  data %>%
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ value,
+                                               .default = .data$Intensity))
 }
 
 impute_mean <- function() {
