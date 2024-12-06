4549ef4b1575fcbf953fec56f7bfd37ffd1cd09a
yasche <172521093+yasche@users.noreply.github.com>
2024-12-06 11:54:15 +0100

    added outline for impute functions

---
+++ R/imputes.R
@@ -0,0 +1,39 @@
+impute_global_lowest <- function() {
+
+}
+
+impute_user_value <- function() {
+
+}
+
+impute_mean <- function() {
+
+}
+
+impute_median <- function() {
+
+}
+
+impute_min <- function() {
+
+}
+
+impute_lod <- function() {
+
+}
+
+impute_knn <- function() {
+
+}
+
+impute_rf <- function() {
+
+}
+
+impute_svd <- function() {
+
+}
+
+impute_qrilc <- function() {
+
+}
