203c0bddded1b192bac94a67a6467cf0594dde94
yasche <172521093+yasche@users.noreply.github.com>
2024-12-06 12:10:35 +0100

    added impute_global_lowest and documentation

--- R/imputes.R
+++ R/imputes.R
@@ -1,5 +1,23 @@
-impute_global_lowest <- function() {
-
+#' Impute missing values by replacing them with the lowest observed intensity (global)
+#'
+#' @description
+#' Replace missing intensity values (`NA`) with the lowest observed intensity. This imputation method is very rudimentary.
+#'
+#' @param data A tidy tibble created by `metamorphr::read_featuretable()`.
+#'
+#' @return A tibble with imputed missing values.
+#' @export
+#'
+#' @examples
+#' toy_metaboscape %>%
+#'   impute_global_lowest()
+#'
+impute_global_lowest <- function(data) {
+  data %>%
+    dplyr::mutate(LoD = min(.data$Intensity, na.rm = T)) %>%
+    dplyr::mutate(Intensity = dplyr::case_when(is.na(.data$Intensity) ~ LoD,
+                                               .default = .data$Intensity)) %>%
+    dplyr::select(-LoD)
 }
 
 impute_user_value <- function() {
