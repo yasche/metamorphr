60335213bbe118fc9ec18e95df8d523693fa549a
yasche <172521093+yasche@users.noreply.github.com>
2025-01-03 16:41:51 +0100

    added test for normalize_median

---
+++ tests/testthat/test-normalize_median.R
@@ -0,0 +1,11 @@
+test_that("normalize_median makes median = 1", {
+  nearones <- toy_metaboscape %>%
+    normalize_median() %>%
+    dplyr::group_by(.data$Sample) %>%
+    dplyr::summarise(Median = median(.data$Intensity, na.rm = TRUE)) %>%
+    #use dplyr::near to account for rounding error
+    dplyr::mutate(nearone = dplyr::near(.data$Median, 1)) %>%
+    dplyr::pull(nearone)
+
+  expect_true(all(nearones))
+})
