cc3e2e45b0197c2a1910fd43a4122d8ed192b9e1
yasche <172521093+yasche@users.noreply.github.com>
2025-01-08 12:05:11 +0100

    added documentation for normalize_quantile_group

--- R/normalize.R
+++ R/normalize.R
@@ -92,6 +92,34 @@ normalize_quantile_all <- function(data)  {
     dplyr::select(-"Rank", -"tmp_Intensity", -"tie")
 }
 
+#' Normalize intensities across samples using grouped Quantile Normalization
+#'
+#' @description
+#' This function performs a Quantile Normalization on each sub-group in the data set. <b>It therefore requires grouping information</b>. See
+#' Examples for more information. This approach might perform better than the standard approach, \code{\link[metamorphr]{normalize_quantile_all}},
+#' if sub-groups are very different (e.g., when comparing cancer vs. normal tissue).
+#'
+#' Other sub-flavors are also available:
+#' - \code{\link[metamorphr]{normalize_quantile_all}}
+#' - \code{\link[metamorphr]{normalize_quantile_batch}}
+#' - \code{\link[metamorphr]{normalize_quantile_smooth}}
+#'
+#' See References for more information.
+#' Note that it is equivalent to the 'Class-specific' normalization in Zhao <i>et al.</i> but has been renamed for internal consistency.
+#'
+#' @param data A tidy tibble created by \code{\link[metamorphr]{read_featuretable}}.
+#' @param group_column Which column should be used for grouping? Usually `grouping_column = Group`. Uses \code{\link[rlang]{args_data_masking}}.
+#'
+#' @return A tibble with intensities normalized across samples.
+#' @export
+#'
+#' @references Y. Zhao, L. Wong, W. W. B. Goh, <i>Sci Rep</i> <b>2020</b>, <i>10</i>, 15534, DOI <a href = "https://doi.org/10.1038/s41598-020-72664-6">10.1038/s41598-020-72664-6</a>.
+#'
+#' @examples
+#' toy_metaboscape %>%
+#' #Metadata, including grouping information, must be added before using normalize_quantile_group()
+#' join_metadata(toy_metaboscape_metadata) %>%
+#' normalize_quantile_group()
 normalize_quantile_group <- function(data, group_column) {
   #also called class-specific; named group to make it consistent
   data %>%
