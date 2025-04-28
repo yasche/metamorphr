a8a609ab02e55085a826c929a1c9cd84d5f7dde0
yasche <172521093+yasche@users.noreply.github.com>
2025-04-28 10:53:09 +0200

    added tests for impue_global_lowest

---
+++ tests/testthat/test-impute_global_lowest.R
@@ -0,0 +1,23 @@
+test_that("NAs were correctly imputed", {
+  min_int <- min(toy_metaboscape$Intensity, na.rm = T)
+
+  nas <- which(is.na(toy_metaboscape$Intensity))
+
+  toy_metaboscape_edit <- toy_metaboscape %>%
+    impute_global_lowest()
+
+  expect_true(all(toy_metaboscape_edit$Intensity[nas] == 1))
+})
+
+test_that("Non-NAs stay unchanged", {
+  non_nas <- which(!is.na(toy_metaboscape$Intensity))
+
+  intensities_before <- toy_metaboscape$Intensity[non_nas]
+
+  toy_metaboscape_edit <- toy_metaboscape %>%
+    impute_global_lowest()
+
+  intensities_after <- toy_metaboscape_edit$Intensity[non_nas]
+
+  expect_equal(intensities_before, intensities_after)
+})
