f2d47c8b8c77bb6f21f663283afceb780b25e898
yasche <172521093+yasche@users.noreply.github.com>
2025-05-07 10:47:16 +0200

    mainly added documentation for plot_volcano

--- R/plot.R
+++ R/plot.R
@@ -1,4 +1,45 @@
-plot_volcano <- function(data, group_column, name_column, groups_to_compare, l2fc_cutoff = 1, p_value_cutoff = 0.05, colors = list(sig_up = "darkred", sig_down = "darkblue", not_sig_up = "grey", not_sig_down = "grey", not_sig = "grey"), adjust_p = FALSE, log2_before = FALSE, return_tbl = FALSE, ...) {
+#' Draws a Volcano Plot or performs calculations necessary to draw one manually
+#'
+#' @description
+#' Performs necessary calculations (i.e., calculate <i>p</i>-values and log<sub>2</sub>-fold changes) and creates a basic <a href = "https://en.wikipedia.org/wiki/Volcano_plot_(statistics)">Volcano Plot</a>.
+#' The plot is drawn with <a href = "https://ggplot2.tidyverse.org/">ggplot2</a> and can therefore be easily manipulated afterwards (e.g., changing the theme or the axis labels).
+#' Please note that the function is intended to be easy to use and beginner friendly and therefore offers limited ability to fine-tune certain parameters of the resulting plot.
+#' If you wish to draw the plot yourself, you can set `return_tbl = TRUE`. In this case, a tibble is returned instead of a ggplot2 object which you can use to create a plot yourself.
+#' A Volcano Plot is used to compare two groups. Therefore grouping information must be provided. See \code{\link[metamorphr]{join_metadata}} for more information.
+#'
+#' @param data A tidy tibble created by \code{\link[metamorphr]{read_featuretable}}.
+#' @param group_column Which column should be used for grouping? Usually `group_column = Group`. Uses \code{\link[rlang]{args_data_masking}}.
+#' @param name_column Which column contains the feature names? Can for example be `name_column = UID` or `name_column = Feature`. Uses \code{\link[rlang]{args_data_masking}}.
+#' @param groups_to_compare Names of the groups which should be compared as a character vector. Those are the group names in the `group_column`. They are usually provided in the form of a metadata tibble and joined via \code{\link[metamorphr]{join_metadata}}.
+#' @param log2fc_cutoff A numeric. What cutoff should be used for the log<sub>2</sub> fold change? Traditionally, this is set to `1` which corresponds to a doubling or halving of intensity or area compared to a control. This is only important for assignment to groups and colors defined in the `colors` argument.
+#' @param p_value_cutoff A numeric. What cutoff should be used for the <i>p</i>-value? Traditionally, this is set to `0.05`. This is only important for assignment to groups and colors defined in the `colors` argument. Note that this is not the -log<sub>10</sub> transformed value.
+#' @param colors A named list for coloring the dots in the Volcano Plot or `NULL` in case the points should not be colored. The list must contain colors for the following groups: `sig_up`, `sig_down`, `not_sig_up`, `not_sig_down` and `not_sig`.
+#' @param adjust_p Should the <i>p</i>-value be adjusted? Can be either `FALSE`, (the default) in case no adjustment should be made or any or the name from \code{\link[stats]{p.adjust.methods}} (e.g., `adjust_p = "fdr"`).
+#' @param log2_before A logical. Should the data be log<sub>2</sub> transformed prior to calculating the <i>p</i>-values?
+#' @param return_tbl A logical. If `FALSE`, returns a ggplot2 object, if `TRUE` returns a tibble which can be used to draw the plot manually to have more control.
+#' @param ... Arguments passed on to \code{\link[stats]{t.test}}. If none are provided (the default), a Welch Two Sample <i>t</i>-test will be performed.
+#'
+#' @return Either a Volcano Plot in the form of a ggplot2 object or a tibble.
+#' @export
+#'
+#' @examples
+#' #returns a Volcano Plot in the form of a ggplot2 object
+#' toy_metaboscape %>%
+#'   impute_lod() %>%
+#'   join_metadata(toy_metaboscape_metadata) %>%
+#'   plot_volcano(group_column = Group,
+#'                name_column = Feature,
+#'                groups_to_compare = c("control", "treatment"))
+#'
+#' #returns a tibble to draw the plot manually
+#' toy_metaboscape %>%
+#'   impute_lod() %>%
+#'   join_metadata(toy_metaboscape_metadata) %>%
+#'   plot_volcano(group_column = Group,
+#'                name_column = Feature,
+#'                groups_to_compare = c("control", "treatment"),
+#'                return_tbl = TRUE)
+plot_volcano <- function(data, group_column, name_column, groups_to_compare, log2fc_cutoff = 1, p_value_cutoff = 0.05, colors = list(sig_up = "darkred", sig_down = "darkblue", not_sig_up = "grey", not_sig_down = "grey", not_sig = "grey"), adjust_p = FALSE, log2_before = FALSE, return_tbl = FALSE, ...) {
   if (log2_before == TRUE) {
     data <- data %>%
       dplyr::mutate(Intensity = log2(.data$Intensity))
@@ -47,10 +88,10 @@ plot_volcano <- function(data, group_column, name_column, groups_to_compare, l2f
       )
 
       data <- data %>%
-        dplyr::mutate(group = dplyr::case_when(.data$p_val <= .env$p_value_cutoff & .data$l2fc <= -.env$l2fc_cutoff ~ "sig_down",
-                                               .data$p_val <= .env$p_value_cutoff & .data$l2fc >= .env$l2fc_cutoff ~ "sig_up",
-                                               .data$p_val > .env$p_value_cutoff & .data$l2fc <= -.env$l2fc_cutoff ~ "not_sig_down",
-                                               .data$p_val > .env$p_value_cutoff & .data$l2fc >= .env$l2fc_cutoff ~ "not_sig_up",
+        dplyr::mutate(group = dplyr::case_when(.data$p_val <= .env$p_value_cutoff & .data$l2fc <= -.env$log2fc_cutoff ~ "sig_down",
+                                               .data$p_val <= .env$p_value_cutoff & .data$l2fc >= .env$log2fc_cutoff ~ "sig_up",
+                                               .data$p_val > .env$p_value_cutoff & .data$l2fc <= -.env$log2fc_cutoff ~ "not_sig_down",
+                                               .data$p_val > .env$p_value_cutoff & .data$l2fc >= .env$log2fc_cutoff ~ "not_sig_up",
                                                .default = "not_sig")) %>%
         dplyr::left_join(colors, by = "group")
 
--- tests/testthat/test-plot_volcano.R
+++ tests/testthat/test-plot_volcano.R
@@ -6,6 +6,38 @@ test_that("returns a ggplot if return_tbl = FALSE", {
   expect_true(ggplot2::is.ggplot(volc_plot))
 })
 
+test_that("returns no ggplot if return_tbl = TRUE", {
+  volc_plot <- toy_metaboscape %>%
+    join_metadata(toy_metaboscape_metadata) %>%
+    plot_volcano(group_column = Group, groups_to_compare = c("control", "treatment"), return_tbl = TRUE)
+
+  expect_false(ggplot2::is.ggplot(volc_plot))
+})
+
+test_that("returns a ggplot if return_tbl = FALSE and no colors are provided", {
+  volc_plot <- toy_metaboscape %>%
+    join_metadata(toy_metaboscape_metadata) %>%
+    plot_volcano(group_column = Group, groups_to_compare = c("control", "treatment"), return_tbl = FALSE, colors = NULL)
+
+  expect_true(ggplot2::is.ggplot(volc_plot))
+})
+
+test_that("returns a no tibble if return_tbl = FALSE", {
+  volc_plot <- toy_metaboscape %>%
+    join_metadata(toy_metaboscape_metadata) %>%
+    plot_volcano(group_column = Group, groups_to_compare = c("control", "treatment"), return_tbl = FALSE)
+
+  expect_false(tibble::is_tibble(volc_plot))
+})
+
+test_that("returns a tibble if return_tbl = TRUE", {
+  volc_tbl <- toy_metaboscape %>%
+    join_metadata(toy_metaboscape_metadata) %>%
+    plot_volcano(group_column = Group, groups_to_compare = c("control", "treatment"), return_tbl = TRUE, colors = NULL)
+
+  expect_true(tibble::is_tibble(volc_tbl))
+})
+
 test_that("throws error if adjust_p method does not exist", {
   expect_error(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% plot_volcano(group_column = Group, groups_to_compare = c("control", "treatment"), return_tbl = FALSE, adjust_p = "asdf"))
 })
