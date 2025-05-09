91ee03249c9660bf9a8f3207aaf819688d167100
yasche <172521093+yasche@users.noreply.github.com>
2025-05-09 15:40:24 +0200

    added scale_center function and documentation

---
+++ R/scale.R
@@ -0,0 +1,31 @@
+#' Center intensities of features around zero
+#'
+#' @description
+#' Centers the intensities of all features around zero using
+#'
+#' \deqn{\widetilde{x}_{ij}=x_{ij}-\overline{x}_{i}}
+#'
+#' where \eqn{\widetilde{x}_{ij}} is the intensity of sample \eqn{j}, feature \eqn{i} after scaling,
+#' \eqn{x_{ij}} is the intensity of sample \eqn{j}, feature \eqn{i} before scaling and \eqn{\overline{x}_{i}} is the mean of intensities of feature \eqn{i} across all samples.
+#' In other words, it subtracts the mean intensity of a feature across samples from the intensities of that feature in each sample.
+#' For more information, see the reference section.
+#'
+#' @param data A tidy tibble created by \code{\link[metamorphr]{read_featuretable}}.
+#'
+#' @return A tibble with intensities scaled around zero.
+#' @export
+#'
+#' @references For further information, see
+#' <ul>
+#' R. A. Van Den Berg, H. C. Hoefsloot, J. A. Westerhuis, A. K. Smilde, M. J. Van Der Werf, <i>BMC Genomics</i> <b>2006</b>, <i>7</i>, 142, DOI <a href = "https://doi.org/10.1186/1471-2164-7-142">10.1186/1471-2164-7-142</a>.
+#' </ul>
+#'
+#' @examples
+#' toy_metaboscape %>%
+#'   scale_center()
+scale_center <- function(data) {
+  data %>%
+    dplyr::group_by(.data$UID) %>%
+    dplyr::mutate(Intensity = .data$Intensity - mean(.data$Intensity, na.rm = TRUE)) %>%
+    dplyr::ungroup()
+}
