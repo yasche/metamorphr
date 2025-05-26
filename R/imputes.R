f0060a830e7360bc3338189df69298272acb87e2
yasche <172521093+yasche@users.noreply.github.com>
2025-05-26 16:53:47 +0200

    added impute_pca function

--- R/imputes.R
+++ R/imputes.R
@@ -328,7 +328,7 @@ impute_rf <- function(data, random_seed = 1L, ...) {
   data_obs_imp
 }
 
-impute_nipals <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
+impute_nipals <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
   # pcaMethods is a bioconductor package so it is not installed with metamorphr if installed via install.packages().
   # check if it installed first
   # also check, if pak is installed
@@ -351,23 +351,42 @@ impute_nipals <- function(data, n_pcs = 2, center = TRUE, scale = "none", direct
   internal_clean_pca_results(data = data, direction = direction, data_list = data_list)
 }
 
-impute_bpca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
+impute_bpca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
+  # pcaMethods is a bioconductor package so it is not installed with metamorphr if installed via install.packages().
+  # check if it installed first
+  # also check, if pak is installed
+  if (!is_installed_wrapper("pcaMethods")) {
+    if (!is_installed_wrapper("pak")) {
+      check_installed_wrapper("pak")
+      check_installed_wrapper("pcaMethods")
+    }
+    check_installed_wrapper("pcaMethods")
+  }
+
+  data_list <- internal_prep_pca_imputes(data = data, direction = direction)
+
+  data <- data_list$data
+
+
+  data <- pcaMethods::pca(data, nPcs = n_pcs, method = "bpca")
+  data <- pcaMethods::completeObs(data)
+
+  internal_clean_pca_results(data = data, direction = direction, data_list = data_list)
+}
+
+impute_ppca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
 
 }
 
-impute_ppca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
+impute_svd <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
 
 }
 
-impute_svd <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
+impute_nlpca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
 
 }
 
-impute_nlpca <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
-
-}
-
-impute_lls <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 1) {
+impute_lls <- function(data, n_pcs = 2, center = TRUE, scale = "none", direction = 2) {
 
 }
 
