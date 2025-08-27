fcb57d4035a5cde7095babe5ea9ebdbed544bdbb
yasche <172521093+yasche@users.noreply.github.com>
2025-08-27 08:15:18 +0200

    Adjust tests for summary_featuretable

--- tests/testthat/test-summary_featuretable.R
+++ tests/testthat/test-summary_featuretable.R
@@ -1,9 +1,9 @@
 test_that("correct number of samples", {
-  expect_output(summary_featuretable(toy_metaboscape), "11 Samples")
+  expect_message(summary_featuretable(toy_metaboscape), "11 Samples")
 })
 
 test_that("correct number of features", {
-  expect_output(summary_featuretable(toy_metaboscape), "10 Features")
+  expect_message(summary_featuretable(toy_metaboscape), "10 Features")
 })
 
 test_that("number of NA and percentage and total number of observations are correct", {
@@ -15,19 +15,19 @@ test_that("number of NA and percentage and total number of observations are corr
 
   calced_tot <- nrow(toy_metaboscape)
 
-  expect_output(summary_featuretable(toy_metaboscape), paste0(as.character(round((calced_na / calced_tot) * 100)), " % missing values"))
-  expect_output(summary_featuretable(toy_metaboscape), paste0(as.character(calced_na), " out of"))
-  expect_output(summary_featuretable(toy_metaboscape), paste0(" out of ", as.character(calced_tot)))
+  expect_message(summary_featuretable(toy_metaboscape), paste0(as.character(round((calced_na / calced_tot) * 100)), " % missing values"))
+  expect_message(summary_featuretable(toy_metaboscape), paste0(as.character(calced_na), " out of"))
+  expect_message(summary_featuretable(toy_metaboscape), paste0(" out of ", as.character(calced_tot)))
 })
 
 test_that("correct number of groups after joining metadata", {
-  expect_output(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "4 Groups")
+  expect_message(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "4 Groups")
 })
 
 test_that("correct number of replicates after joining metadata", {
-  expect_output(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "Replicates detected: 1...3")
+  expect_message(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "Replicates detected: 1...3")
 })
 
 test_that("correct number of batches after joining metadata", {
-  expect_output(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "1 Batches: 1")
+  expect_message(toy_metaboscape %>% join_metadata(toy_metaboscape_metadata) %>% summary_featuretable(), "1 Batches: 1")
 })
