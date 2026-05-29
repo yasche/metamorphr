65834229b35458636b58572cbdfddc4a9a8c17ac
yasche <172521093+yasche@users.noreply.github.com>
2026-05-29 16:32:14 +0200

    `convert_from_wide` checks if `data` is a tibble/df and throws informative error otherwise

--- R/convert.R
+++ R/convert.R
@@ -1,7 +1,32 @@
+#' Convert a wide feature table to a tidy tibble
+#'
+#' @description
+#' Feature tables are usually stored in a "wide" data format where sample names are stored in columns and features are stored in rows.
+#' This functions transforms those feature tables into a "long" and tidy data format to use it with functions provided in the `metamorphr` package.
+#' `convert_from_wide` works with tibbles and data frames. To convert a matrix, see \code{\link[metamorphr]{convert_from_matrix}}.
+#'
+#' @param data A feature table data frame or tibble in wide format. To convert a matrix, see \code{\link[metamorphr]{convert_from_matrix}}.
+#' @param label_col The index or name of the column that will be used to label Features. For example an identifier (_e.g._, KEGG, CAS, HMDB) or a _m/z_-RT pair.
+#' @param metadata_cols The index/indices or name(s) of column(s) that hold additional feature metadata (_e.g._, retention times, additional identifiers or _m/z_ values).
+#'
+#' @returns A tidy tibble.
+#' @export
+#'
+#' @examples
+#' system.file("extdata", "toy_metaboscape.csv", package = "metamorphr"
 convert_from_wide <- function(data, label_col = 1, metadata_cols = NULL) {
   # perform some checks
   if (length(label_col) > 1) {
     rlang::abort("label_col must be of length 1.")
+  }
+
+  if (!is.data.frame(data)) {
+    if (!is.matrix(data)) {
+      rlang::abort(paste0("`data` must be of class `tibble` or `data.frame`, not `", paste0(class(data), collapse = ", "), "`."))
+    } else {
+      rlang::abort("`data` must be of class `tibble` or `data.frame`, not `matrix`. To convert a matrix use function `convert_from_matrix`.")
+    }
+
   }
 
   data_colnames <- colnames(data)
@@ -37,4 +62,8 @@ convert_from_wide <- function(data, label_col = 1, metadata_cols = NULL) {
     dplyr::mutate(Intensity = as.numeric(.data$Intensity)) %>%
     # replace 0 with NA
     dplyr::mutate(Intensity = dplyr::na_if(.data$Intensity, 0))
+}
+
+convert_from_matrix <- function(data, samples_in_cols = TRUE) {
+
 }
