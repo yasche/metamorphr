e494937eaca95616137949f3c4e69f4063f263e9
yasche <172521093+yasche@users.noreply.github.com>
2026-06-01 13:35:29 +0200

    Change internals of `convert_from_wide`

--- R/convert.R
+++ R/convert.R
@@ -34,18 +34,18 @@ convert_from_wide <- function(data, label_col = 1, metadata_cols = NULL) {
 
   data_colnames <- colnames(data)
 
-  # functionality for when label_col is a character
-  if (is.character(label_col)) {
-    label_col <- which(data_colnames == label_col)
+  # functionality for when label_col is a numeric
+  if (is.numeric(label_col)) {
+    label_col <- data_colnames[label_col]
   }
 
-  # functionality for when metadata_cols is a character
-  if (all(!(is.null(metadata_cols)), is.character(metadata_cols))) {
-    metadata_cols <- which(data_colnames %in% metadata_cols)
+  # functionality for when metadata_cols is a numeric
+  if (all(!(is.null(metadata_cols)), is.numeric(metadata_cols))) {
+    metadata_cols <- data_colnames[metadata_cols]
   }
 
   # 1: always UID
-  metadata_cols <- c(1, metadata_cols + 1, label_col + 1)
+  metadata_cols <- c("UID", metadata_cols, "Feature")
 
   metadata_cols <- unique(metadata_cols)
 
@@ -53,13 +53,11 @@ convert_from_wide <- function(data, label_col = 1, metadata_cols = NULL) {
   data %>%
     tibble::as_tibble() %>%
     dplyr::rename("Feature" = dplyr::all_of(label_col)) %>%
-    # select(- {{ drop_cols }}) %>%
     dplyr::mutate(Feature = as.character(.data$Feature)) %>%
     dplyr::mutate(UID = seq(1, length(.data$Feature))) %>%
     dplyr::relocate("UID", .before = 1) %>%
     dplyr::relocate("Feature", .after = 1) %>%
-    # print()
-    tidyr::gather(-dplyr::all_of(metadata_cols), key = "Sample", value = "Intensity") %>%
+    tidyr::gather(-dplyr::any_of(metadata_cols), key = "Sample", value = "Intensity") %>%
     dplyr::relocate("Sample", .after = 2) %>%
     dplyr::relocate("Intensity", .after = 3) %>%
     dplyr::mutate(Intensity = as.numeric(.data$Intensity)) %>%
