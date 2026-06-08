b70bee231fb0c930f700754d30d9398e98f9ce1b
yasche <172521093+yasche@users.noreply.github.com>
2026-06-08 10:20:50 +0200

    Add tests for `remove_empty_cols`

--- tests/testthat/test-remove_empty_cols.R
+++ tests/testthat/test-remove_empty_cols.R
@@ -83,3 +83,118 @@ test_that("message is as expected", {
     "The following columns were removed: `if`, `<-`."
   )
 })
+
+
+
+test_that("`always_keep` argument works as expected with numeric, character and non-sntactic", {
+  test_tibble1 <- tibble::tibble(a = c(1, 2, 3), b = c(NA, 2, 3), c = c(NA, NA, 3), d = c(NA, NA, NA))
+  test_tibble2 <- tibble::tibble(a = c(1, 2, 3), b = c(NA, NA, NA), c = c(NA, NA, 3), d = c(NA, NA, NA))
+  test_tibble3 <- tibble::tibble(a = c(1, 2, 3), b = c(1, 2, 3), c = c(1, 2, 3), d = c(1, 2, 3))
+
+  test_tibble4 <- tibble::tibble(a = c("a", "b", "c"), b = c(NA, "b", "c"), c = c(NA, NA, "c"), d = c(NA, NA, NA))
+  test_tibble5 <- tibble::tibble(a = c("a", "b", "c"), b = c(NA, NA, NA), c = c(NA, NA, "c"), d = c(NA, NA, NA))
+  test_tibble6 <- tibble::tibble(a = c("a", "b", "c"), b = c("a", "b", "c"), c = c("a", "b", "c"), d = c("a", "b", "c"))
+
+  test_tibble_nonsynt <- tibble::tibble(`123` = c("a", "b", "c"), `_ABC` = c(NA, "b", "c"), `if` = c(NA, NA, NA), `<-` = c(NA, NA, NA))
+
+  expect_equal(
+    remove_empty_cols(test_tibble1, show_removed_cols = FALSE, always_keep = d),
+    test_tibble1
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = d),
+    dplyr::select(test_tibble2, -b)
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = c(b, d)),
+    test_tibble2
+  )
+
+
+  expect_equal(
+    remove_empty_cols(test_tibble4, show_removed_cols = FALSE, always_keep = d),
+    test_tibble4
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = d),
+    dplyr::select(test_tibble5, -b)
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = c(b, d)),
+    test_tibble5
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble1, show_removed_cols = FALSE, always_keep = "d"),
+    test_tibble1
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = "d"),
+    dplyr::select(test_tibble2, -b)
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = c("b", "d")),
+    test_tibble2
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = c(b, "d")),
+    test_tibble2
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = c(2, d)),
+    test_tibble2
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = c(2, d)),
+    test_tibble2
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble4, show_removed_cols = FALSE, always_keep = "d"),
+    test_tibble4
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = "d"),
+    dplyr::select(test_tibble5, -b)
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = c("b", "d")),
+    test_tibble5
+  )
+
+
+  expect_equal(
+    remove_empty_cols(test_tibble4, show_removed_cols = FALSE, always_keep = 4),
+    test_tibble4
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = 4),
+    dplyr::select(test_tibble5, -b)
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = c(2, 4)),
+    test_tibble5
+  )
+
+  expect_equal(
+    remove_empty_cols(test_tibble_nonsynt, show_removed_cols = FALSE, always_keep = c(`<-`, `if`)),
+    test_tibble_nonsynt
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble5, show_removed_cols = FALSE, always_keep = c(4, 2)),
+    test_tibble5
+  )
+  expect_equal(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = NULL),
+    dplyr::select(test_tibble2, -b, -d)
+  )
+  expect_error(
+    remove_empty_cols(test_tibble2, show_removed_cols = FALSE, always_keep = THIS_COLUMN_DOESNT_EXIST),
+    "Can't select columns that don't exist."
+  )
+})
+
+
