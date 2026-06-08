f112aceb05b979657f18137ac784e360946a41bd
yasche <172521093+yasche@users.noreply.github.com>
2026-06-08 14:14:58 +0200

    Add tests for `read_featuretable`, `remove_empty_cols = TRUE`

--- tests/testthat/test-read_featuretable.R
+++ tests/testthat/test-read_featuretable.R
@@ -57,3 +57,43 @@ test_that("read_featuretable() throws an error if length(label_col) > 1", {
 test_that("read_featuretable() throws an error if length(label_col) > 1", {
   expect_error(read_featuretable(test_path("data", "test_read_featuretable.csv"), label_col = c("a", "b")))
 })
+
+test_that("`remove_empty_cols` argument works as expected", {
+  expect_equal(
+    read_featuretable(test_path("data", "test_read_featuretable_empty_cols.csv"),
+                      label_col = 3,
+                      metadata_cols = c(1:2),
+                      remove_empty_cols = T,
+                      show_removed_cols = F),
+    test_read_featuretable
+  )
+
+  expect_message(
+    read_featuretable(test_path("data", "test_read_featuretable_empty_cols.csv"),
+                      label_col = 3,
+                      metadata_cols = c(1:2),
+                      remove_empty_cols = T,
+                      show_removed_cols = T),
+    "The following columns were removed: `empty_col`, `more_empty_col`.")
+
+  expect_equal(
+    read_featuretable(test_path("data", "test_read_featuretable_empty_cols.csv"),
+                      label_col = 3,
+                      metadata_cols = c(1:2, 7:8),
+                      remove_empty_cols = F,
+                      show_removed_cols = F),
+    dplyr::bind_cols(test_read_featuretable,
+                     tibble::tibble(
+                       empty_col = rep(NA, 6),
+                       more_empty_col = rep(NA, 6)
+                     ))
+  )
+
+  expect_true(
+    all(is.na(read_featuretable(test_path("data", "test_read_featuretable_empty_cols.csv"),
+                      label_col = 7,
+                      metadata_cols = c(1:3),
+                      remove_empty_cols = T,
+                      show_removed_cols = F)$Feature))
+  )
+})
